f2c89b72efeff86951b9fbf1f5988a0e867978f0
haikel11 <hatemiii91@gmail.com>
2023-03-02 01:25:41 +0100

    update file

--- Fast_API/test.py
+++ Fast_API/test.py
@@ -1,16 +1,16 @@
 import requests
 
-response = requests.post("http://localhost:4000/predict", json={
-  "model_key": "BMW",
-  "mileage":75515,
-  "engine_power":135,
+response = requests.post("https://fast-tryout.herokuapp.com/predict/", json={
+  "model_key": "Audi",
+  "mileage":151555,
+  "engine_power":230,
   "fuel": "diesel",
-  "paint_color": "grey",
-  "car_type": "suv",
-  "private_parking_available": False,
+  "paint_color": "blue",
+  "car_type": "estate",
+  "private_parking_available": True,
   "has_gps": True,
-  "has_air_conditioning": False,
-  "automatic_car": True,
+  "has_air_conditioning": True,
+  "automatic_car": False,
   "has_getaround_connect": True,
   "has_speed_regulator": False,
   "winter_tires": True
